5aeee78e51bf30851ccb1b7693d57ab4ccf7ec2c
prince-patel0809 <propatel0809@gmail.com>
2025-08-15 23:20:29 +0530

    Set DEBUG to True for development and add CSRF trusted origins

--- tweetest/tweetest/settings.py
+++ tweetest/tweetest/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sl8l3lz4=kgl9t46o9i!qx5ip8h_4heqrqrko&s5&q$h+6^y2('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -137,3 +137,7 @@ LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/tweet/'
 
 LOGOUT_REDIRECT_URL = '/tweet/'
+
+CSRF_TRUSTED_ORIGINS=[
+    'https://tweet-242j.onrender.com'
+]
